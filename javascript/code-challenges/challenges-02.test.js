b7407ab045b8abaad6827ed5293f50f5b18936c9
rpruazol <rpruazol@gmail.com>
2022-07-17 20:50:04 -0400

    stretch goals

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -109,7 +109,13 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 
 const evenOdd = (arr) => {
   // Solution code here...
-
+  return arr.map(value => {
+    if(typeof value !== 'number'){
+      return 'N/A'
+    } else {
+      return value % 2 === 0 ? 'even' : 'odd';
+    }
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -156,6 +162,7 @@ const snorlaxAbilities = {
 
 const extractAbilities = (arr) => {
   // Solution code here...
+  return arr.map(value => value.ability.name);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -203,6 +210,14 @@ const snorlaxStats = {
 
 const extractStats = (arr) => {
   // Solution code here...
+  return arr.map(value => {
+    return  (
+      {
+        "name": value.stat.name,
+        "total": value.effort + value.baseStat
+      }
+    )
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -274,7 +289,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return an array containing the keys from an object', () => {
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
