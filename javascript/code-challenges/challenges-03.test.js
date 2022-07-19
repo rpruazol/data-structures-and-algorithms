b71ae17dc96137a861acd883722fba00ceedd707
rpruazol <rpruazol@gmail.com>
2022-07-19 00:05:06 -0400

    stretch 8

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -184,6 +184,11 @@ const characters = [
 
 const getCharactersWithoutChildren = (arr) => {
   // Solution code here...
+  return arr.filter(value => {
+    if(!value.children){
+      return true;
+    }
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -298,7 +303,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return an array containing characters who do not have children', () => {
     expect(getCharactersWithoutChildren(characters)).toStrictEqual([ { name: 'Sansa', spouse: 'Tyrion', house: 'Stark' }, { name: 'Jon', spouse: null, house: 'Snow' } ]);
     expect(getCharactersWithoutChildren(characters).length).toStrictEqual(2);
