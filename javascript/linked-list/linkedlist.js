4bcecef6b3ed8678ca11026a5c5cb1c793ef651b
rpruazol <rpruazol@gmail.com>
2023-07-20 23:35:30 -0400

    moved dummyll

--- javascript/linked-list/linkedlist.js
+++ javascript/linked-list/linkedlist.js
@@ -6,14 +6,6 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
-  traverse() {
-    let current = this.head;
-    while (current) {
-      current = current.next;
-    }
-    // should we return something?
-
-  }
   insert(value) {
     // add new node with the value to the head of the list with an O(1) time performance
     let newNode = new Node(value);
