269780b25ca6a0bff9aa0069be1fd68d4156455e
rpruazol <rpruazol@gmail.com>
2023-07-20 22:35:21 -0400

    test file refactor

---
+++ javascript/linked-list/__tests__/dummyll.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { LinkedList, Node } = require('../linkedlist');
+
+
+
+
+const dummyll = new LinkedList();
+dummyll.insert('a linked list');
+dummyll.insert('is');
+dummyll.insert('this');
+
+// {this}->{is}->{a linked list}->NULL
+
+module.exports = {dummyll};
--- javascript/linked-list/__tests__/linkedlist.test.js
+++ javascript/linked-list/__tests__/linkedlist.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { LinkedList, Node } = require('../linkedlist');
+const {dummyll} = require('./dummyll');
 
 // Can successfully instantiate an empty linked list
 // Can properly insert into the linked list
@@ -11,10 +12,6 @@ const { LinkedList, Node } = require('../linkedlist');
 // Can properly return a collection of all the values that exist in the linked list
 
 
-const testLL = new LinkedList();
-testLL.insert('a linked list');
-testLL.insert('is');
-testLL.insert('this');
 
 
 
@@ -24,38 +21,47 @@ describe('linked list', () => {
     expect(emptyLL.head).toEqual(null);
   });
   test('Can properly insert into the linked list', () => {
+    const testLL = new LinkedList(dummyll.head);
     expect(testLL.head.value).toEqual('this');
     expect(testLL.head.next.value).toEqual('is');
   }),
   test('The head property will properly point to the first node in the linked list', () => {
+    const testLL = new LinkedList(dummyll.head);
     expect(testLL.head.value).toEqual('this');
   }),
   test('Can properly insert multiple nodes into the linked list', () => {
+    const testLL = new LinkedList(dummyll.head);
     expect(testLL.head.value).toEqual('this');
     expect(testLL.head.next.value).toEqual('is');
     expect(testLL.head.next.next.value).toEqual('a linked list');
   }),
   test('Will return true when finding a value within the linked list that exists', () => {
+    const testLL = new LinkedList(dummyll.head);
     expect(testLL.includes('is')).toEqual(true);
   }),
   test('Will return false when searching for a value in the linked list that does not exist', () => {
+    const testLL = new LinkedList(dummyll.head);
     expect(testLL.includes('bananas')).toEqual(false);
   }),
   test('Can properly return a collection of all the values that exist in the linked list', () => {
-    //
+    const testLL = new LinkedList(dummyll.head);
     expect(testLL.toString()).toEqual('{this}->{is}->{a linked list}->NULL');
   });
   test('Can append a new node with a given value to the end of the list', () => {
+    const testLL = new LinkedList(dummyll.head);
+    console.log(testLL);
     testLL.append('last node');
     expect(testLL.toString()).toEqual('{this}->{is}->{a linked list}->{last node}->NULL');
   });
   test('Can insert a new node with a given value immediately before the first node that has the value specified', () => {
     // BEFORE {this}->{is}->{a linked list}->{last node}->NULL
+    const testLL = new LinkedList(dummyll.head);
     testLL.insertBefore('so', 'this');
     testLL.insertBefore('definitely', 'a linked list');
     expect(testLL.toString()).toEqual('{so}->{this}->{is}->{definitely}->{a linked list}->{last node}->NULL');
   });
   test('Can insert a new node with a given value immediately AFTER the first node that has the value specified', () => {
+    const testLL = new LinkedList(dummyll.head);
     testLL.insertAfter('I\'m sure', 'so');
     testLL.insertAfter('NOT', 'definitely');
     expect(testLL.toString()).toEqual('{so}->{I\'m sure}->{this}->{is}->{definitely}->{NOT}->{a linked list}->{last node}->NULL');
