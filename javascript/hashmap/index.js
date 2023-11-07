fc18ae3f5630d5d18125df9acb29137313758142
rpruazol <rpruazol@gmail.com>
2023-11-06 23:54:44 -0500

    workflow

---
+++ javascript/hashmap/index.js
@@ -0,0 +1,10 @@
+
+const {LinkedList} = require('../linked-list/linkedlist');
+
+class HashTable {
+  constructor()
+
+  hash(){
+    // this function will look at a key and "evaluate" it.  will return the location in the array index where we will store the value.  evaluate in this case will 
+  }
+}
