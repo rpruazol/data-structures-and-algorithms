52d5a377cc003671adc90de69ad68446fda848a2
rpruazol <rpruazol@gmail.com>
2023-11-07 19:06:13 -0500

    breadth first traversal

--- javascript/graphs/graph.test.js
+++ javascript/graphs/graph.test.js
@@ -15,4 +15,22 @@ describe('graph implementation', () => {
     expect(newGraph.getVerticies().length).toBe(2);
     expect(newGraph.size()).toBe(2);
   })
+  test('breadth-first traversal', () => {
+    const graph = new Graph();
+    graph.addVertex('A');
+    graph.addVertex('B');
+    graph.addVertex('C');
+    graph.addVertex('D');
+    const verticies = graph.getVerticies()
+    const a = verticies[0]
+    const b = verticies[1]
+    const c = verticies[2]
+    const d = verticies[3]
+    graph.addEdge(a, b);
+    graph.addEdge(a, c);
+    graph.addEdge(b, d);
+
+    const result = graph.breadthFirstTraversal(a);
+    expect(result).toEqual(['A', 'B', 'C', 'D']); // The expected traversal order
+  })
 })
